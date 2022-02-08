327569831322357a1adf3e420a70fa7eb2d47034
Azie Melza Pratama <aziemp66@gmail.com>
2022-02-08 22:49:15 +0700

    Testing Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-    return <div>Hemlo</div>;
+    return (
+        <div>
+            <Route path="/">
+                <AllMeetupsPage />
+            </Route>
+            <Route path="/new-meetup">
+                <NewMeetupsPage />
+            </Route>
+            <Route path="/favorites">
+                <FavoritesPage />
+            </Route>
+        </div>
+    );
 }
 
 export default App;
